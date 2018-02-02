64fb8d206ace7cbf14914edca799acd409c41b66
Natalia Kisanga <nkisanga@uchicago.edu>
2018-02-02 15:52:37 -0600

    adding actions to buttons

--- app/src/main/java/team2/falldetection/MainActivity.java
+++ app/src/main/java/team2/falldetection/MainActivity.java
@@ -1,13 +1,31 @@
 package team2.falldetection;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /** Called when the user taps the START button */
+    public void startRecording(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
     }
 }
