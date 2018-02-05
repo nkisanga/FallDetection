338cd1dddaa96255a07b5f6c1bb0ad7a904653ff
Natalia Kisanga <nkisanga@uchicago.edu>
2018-02-05 00:05:55 -0600

    trying to implement accelerometer tracking

--- app/src/main/java/team2/falldetection/MainActivity.java
+++ app/src/main/java/team2/falldetection/MainActivity.java
@@ -10,16 +10,51 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.falldetection.MESSAGE";
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private TextView xText, yText, zText;
+    private SensorManager SM;
+    private Sensor mySensor;
+
+    //public static final String EXTRA_MESSAGE = "com.example.falldetection.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create our sensor manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        // Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Register Sensor Listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Assign Text Views
+        xText = (TextView)findViewById(R.id.xText);
+        //yText = (TextView)findViewById(R.id.yText);
+        //zText = (TextView)findViewById(R.id.zText);
+
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not in use
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        //xText.setText("X: " + event.values[0]);
+        //yText.setText("Y: " + event.values[1]);
+        //zText.setText("Z: " + event.values[2]);
+    }
+
 
     /** Called when the user taps the START button */
     public void startRecording(View view) {
