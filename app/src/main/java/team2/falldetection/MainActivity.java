abc1fe6060d20cd04378132e3f1b24119f81d7da
Natalia Kisanga <nkisanga@uchicago.edu>
2018-02-06 21:52:56 -0600

    Built app to display accelerometer values. Works! :)

--- app/src/main/java/team2/falldetection/MainActivity.java
+++ app/src/main/java/team2/falldetection/MainActivity.java
@@ -1,68 +1,23 @@
 package team2.falldetection;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.widget.TextView;
+public class MainActivity extends Activity {
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
-
-    private TextView xText, yText, zText;
-    private SensorManager SM;
-    private Sensor mySensor;
-
-    //public static final String EXTRA_MESSAGE = "com.example.falldetection.MESSAGE";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // create our sensor manager
-        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
-
-        // Accelerometer Sensor
-        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        // Register Sensor Listener
-        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        // Assign Text Views
-        xText = (TextView)findViewById(R.id.xText);
-        //yText = (TextView)findViewById(R.id.yText);
-        //zText = (TextView)findViewById(R.id.zText);
-
     }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // not in use
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        //xText.setText("X: " + event.values[0]);
-        //yText.setText("Y: " + event.values[1]);
-        //zText.setText("Z: " + event.values[2]);
-    }
-
 
     /** Called when the user taps the START button */
-    public void startRecording(View view) {
+    public void StartRecordingActivity(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, StartRecordingActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.editText);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
 }
