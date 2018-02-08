f03b55efcf9611c5bb642865d74c2b1fffe8c1d1
Natalia Kisanga <nkisanga@uchicago.edu>
2018-02-08 00:08:37 -0600

    Attempts at writing accelerometer data to file. Not functioning correctly yet.

--- app/src/main/java/team2/falldetection/MainActivity.java
+++ app/src/main/java/team2/falldetection/MainActivity.java
@@ -1,16 +1,33 @@
 package team2.falldetection;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends Activity {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private FileWriter writer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     /** Called when the user taps the START button */
@@ -18,6 +35,82 @@ public class MainActivity extends Activity {
         // Do something in response to button
         Intent intent = new Intent(this, StartRecordingActivity.class);
         startActivity(intent);
+    }
+
+    public void onStartClick(View view) {
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onStopClick(View view) {
+        mSensorManager.unregisterListener(this);
+    }
+
+    /*
+    public void wrtieFileOnInternalStorage(Context mcoContext, String sFileName, String sBody){
+        File file = new File(mcoContext.getFilesDir(),"mydir");
+        if(!file.exists()){
+            file.mkdir();
+        }
+
+        try{
+            File gpxfile = new File(file, sFileName);
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(sBody);
+            writer.flush();
+            writer.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+
+    */
+
+    protected void onResume() {
+        super.onResume();
+
+        try {
+            writer = new FileWriter("fall_detection_file.txt", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        if(writer != null) {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        String x_str = String.valueOf(x);
+        String y_str = String.valueOf(y);
+        String z_str = String.valueOf(z);
+
+        try {
+            writer.write(x_str+","+y_str+","+z_str+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
